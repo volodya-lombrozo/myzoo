c4e9875cef9591352966463f08b606a6ff4bf3c2
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2021-10-12 15:29:25 +0300

    chore: unworked version of zookeeper discovery

--- spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/MicronautVisitor.java
+++ spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/MicronautVisitor.java
@@ -9,14 +9,14 @@ import java.util.logging.Logger;
 @Component
 public class MicronautVisitor {
 
-
     private final RestTemplate template = new RestTemplate();
     private final Logger logger = Logger.getLogger("MicronautVisitor Scheduler");
+    private String url = "http://micro:8080/";
 
     @Scheduled(fixedRate = 1000)
     public void goToMicronautServer() {
-        logger.info("Try to call micronaut server");
-        template.postForEntity("http://micro:8080/", "", String.class);
+        logger.info(String.format("Try to call micronaut server by url %s", url));
+        template.postForEntity(url, "", String.class);
     }
 
 }
