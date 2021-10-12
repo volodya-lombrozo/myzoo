0ab345f3dc287f3c147dcd6ca4f6a8ca9a349e51
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2021-10-12 13:45:13 +0300

    chore: add spring based application

---
+++ spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/Application.java
@@ -0,0 +1,14 @@
+package com.github.volodya_lombrozo.spring_based;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+}
---
+++ spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/MainController.java
@@ -0,0 +1,20 @@
+package com.github.volodya_lombrozo.spring_based;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class MainController {
+
+    private final long id = Math.abs(new Random().nextLong());
+
+    @GetMapping
+    public String welcome() {
+        return String.format("Welcome to Spring Based Application%n" +
+                "This is a server application%n" +
+                "My id is %s%n", id);
+    }
+
+}
