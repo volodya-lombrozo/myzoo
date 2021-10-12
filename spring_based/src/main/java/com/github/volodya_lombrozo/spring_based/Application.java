72c42c514fd6ec08831f73cfc6653b2fa7c5096b
volodya-lombrozo <volodya.lombrozo@yandex.ru>
2021-10-12 19:00:04 +0300

    feat: connect spring application to zookeeper

--- spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/Application.java
+++ spring_based/src/main/java/com/github/volodya_lombrozo/spring_based/Application.java
@@ -2,15 +2,15 @@ package com.github.volodya_lombrozo.spring_based;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
-
-
 }
